8f59a939b69262ee1e648bb796f2138c9fe564c4
Rohit Pillai <rohit.pillai@clarivate.com>
2022-09-29 12:23:36 +0530

    app module updated

--- foodAppFrontEnd/src/app/app.module.ts
+++ foodAppFrontEnd/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent
   ],
   imports: [
     BrowserModule,
